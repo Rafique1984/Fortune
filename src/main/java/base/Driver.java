8eb53bac4f3ac0e43e457994710025fb6a2c6a8b
Mohammad Islam <rafi420du@gmail.com>
2018-03-08 14:48:29 -0500

    functionality add on drivers, Test base,saucelab and read properties class

--- src/main/java/base/Driver.java
+++ src/main/java/base/Driver.java
@@ -1,7 +1,35 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import utils.ReadProperties;
 
 public class Driver extends ReadProperties {
+	  public static WebDriver driver;
+	 
+	  public void setDriver(){
+		if(getProperty("browser").equalsIgnoreCase("firfox")){
+			initFirefox();
+		} else if(getProperty("browser").equalsIgnoreCase("Chrome")){
+			initChrome();
+		} else {
+			initFirefox();
+			
+		}
+		
+	}
+	public void initChrome(){
+		driver = new ChromeDriver();
+	}
+	public void initFirefox(){
+		driver = new FirefoxDriver();
+	}
+	public void setDriverProperty(){
+		driver.manage().window().maximize();
+		driver.get(getProperty("appUrl"));
+		
+	}
 
 }
--- src/main/java/utils/ReadProperties.java
+++ src/main/java/utils/ReadProperties.java
@@ -1,5 +1,46 @@
 package utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
 public class ReadProperties {
+	
+	public  String getProperty(String key){
+		
+		String value = null;
+		Properties prop = loadproperty("testRun");		
+		value=prop.getProperty(key);
+		if(value == null){
+			String currentEnv = prop.getProperty("testEnv");
+			prop = loadproperty(currentEnv);
+			value=prop.getProperty(key);
+			
+		}
+		
+		return value;
+	}
+	public  String getSaucelabsProperties(String key){
+		String  value = null;
+		Properties prop = loadproperty("testRun");		
+		value=prop.getProperty(key);
+		
+		return value;
+	}
+
+	private  Properties loadproperty(String propFilename){
+		Properties prop = new Properties();
+		
+		String filepath = "./src/main/resources/properties/"+propFilename+".properties";
+		try {
+			File file = new File(filepath);
+			FileInputStream stream = new FileInputStream(file);
+			prop.load(stream);
+		} catch (Exception e) {
+						e.printStackTrace();
+		}
+		return prop;
+	}
 
 }
